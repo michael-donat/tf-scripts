5db7c90bdb9b159669b40564db40a690e1fbbecb
Michael Donat <thornag@gmail.com>
2013-06-26 14:59:08 +0100

    wip

--- bin/buildsubs.py
+++ bin/buildsubs.py
@@ -22,14 +22,14 @@ class Callbacks:
 /set WRsign=@{BxCyellow}(@{BxCred}~WR~@{BxCyellow})@{n}
 /def _WR_player_sub = \\
     /substitute -p @{}%{PL}%{P1}%{WRsign}%{P4- }%{PR}%;\\
-    /if ({P4}=~"." | {P4}=~",") \
+    /if ({P3}=~"." | {P3}=~",") \
         /let target=$(/_odmien_M_B %{P1})%;\\
         /eval /def key_f6 = /zabij %{target}%;\\
     /endif
 """
 
     def lineWR(self, p1, p2, r, i):
-        return "/def -Fp3 -mregexp -t'(("+p1[:1].lower()+"|"+p1[:1].upper()+")"+p1[1:]+" "+p2+" (mutant|mutantka|"+r+")|"+i+")([*,.\] ]|$)' _player_sub_"+str(self.counter)+" = /_WR_player_sub"
+        return "/def -Fp3 -mregexp -t'(("+p1[:1].lower()+"|"+p1[:1].upper()+")"+p1[1:]+" "+p2+" (?:mutant|mutantka|"+r+")|"+i+")([*,.\] ]|$)' _player_sub_"+str(self.counter)+" = /_WR_player_sub"
 
     def lineOK(self, p1, p2, r, i):
         return "/def -Fp3 -mregexp -t'(("+p1[:1].lower()+"|"+p1[:1].upper()+")"+p1[1:]+" "+p2+" "+r+"|"+i+")([*,.\] ]|$)' _player_sub_"+str(self.counter)+" = /_OK_player_sub"
@@ -38,7 +38,7 @@ class Callbacks:
         return "/set MCsign=@{BxCyellow}(@{BxCred}~MC~@{BxCyellow})@{n}"
 
     def lineMC(self, p1, p2, r, i):
-        return "/def -Fp3 -mregexp -t'(("+p1[:1].lower()+"|"+p1[:1].upper()+")"+p1[1:]+" "+p2+" (mutant|mutantka|"+r+")|"+i+")([*,.\] ]|$)' _player_sub_"+str(self.counter)+" = /substitute -p @{}%{PL}%{P1}%{MCsign}%{P4- }%{PR}"
+        return "/def -Fp3 -mregexp -t'(("+p1[:1].lower()+"|"+p1[:1].upper()+")"+p1[1:]+" "+p2+" (?:mutant|mutantka|"+r+")|"+i+")([*,.\] ]|$)' _player_sub_"+str(self.counter)+" = /substitute -p @{}%{PL}%{P1}%{MCsign}%{P4- }%{PR}"
 
     def headerOHM(self):
         return "/set OHMsign=[OHM] @{n}"
@@ -57,6 +57,11 @@ class Callbacks:
 /set OKsign=@{BxCyellow}(@{BxCred}~OK~@{BxCyellow})@{n}
 /def _OK_player_sub = \\
     /substitute -p @{}%{PL}%{P1}%{OKsign}%{P4- }%{PR}%;\\
+    /echo 1: %{P1}%;\\
+    /echo 2: %{P2}%;\\
+    /echo 3: %{P3}%;\\
+    /echo 4: %{P4}%;\\
+    /echo 5: %{P5}%;\\
     /if ({P4}=~"." | {P4}=~",") \
         /let target=$(/_odmien_M_B %{P1})%;\\
         /eval /def key_f6 = /zabij %{target}%;\\
