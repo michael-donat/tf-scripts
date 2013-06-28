40d7ed11008fe8a752f11121507631fa5ece110c
Michal Donat <thornag@gmail.com>
2013-06-28 17:41:57 +0100

    warbif fix

--- lib/combat/system.tf
+++ lib/combat/system.tf
@@ -437,7 +437,7 @@
 /def prompt_PVP = \
     /if ({1}=~"." | {1}=~"," | {1} =~"]," | {1} =~"].") \
         /let target=$(/_odmien_M_B %{-1})%;\
-        /eval /def key_f19 = /zabij %{target}%;przedstaw %{target}\
+        /eval /def key_f19 = /zabij %{target}%%%;przedstaw %{target}%;\
         /echo -p @{Cbgred}# # # # # # # # #    S T O P     S T O P    # # # # # # # # #%;\
         /echo -p @{Cbgred} F19  F19  F19  F19  F19    %{target}    F19  F19  F19  F19  F19%;\
     /endif
