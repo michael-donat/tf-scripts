e43a225c4dd473f93f75c76a3e894c85733018f3
Michal Donat <thornag@gmail.com>
2013-08-26 21:38:28 +0100

    gubienie sie przy panice w rinde

--- lib/combat/exp/rinde.tf
+++ lib/combat/exp/rinde.tf
@@ -9,39 +9,41 @@
 /def klucz = ob plaskorzezbe
 /def kluczz = ob pierwsza plaskorzezbe%;ob druga plaskorzezbe%;ob trzecia plaskorzezbe%;ob czwarta plaskorzezbe%;ob piata plaskorzezbe
 
-/def -msubstr -t'polnoc, poludniowy-zachod, wschod i poludnie' -n1 _rinde_klapa_kierunek1 = \
-    /test echo(decode_attr("          #    "), "Cwhite") %;\
-	/test echo(decode_attr("           #   "), "Cwhite") %;\
-	/test echo(decode_attr("    #########  "), "Cwhite") %;\
-	/test echo(decode_attr("           #   "), "Cwhite") %;\
-	/test echo(decode_attr("          #    "), "Cwhite") %;\
-	/purge _rinde_klapa_kierunek*
+/def klapa = \
+    /def -msubstr -t'polnoc, poludniowy-zachod, wschod i poludnie' -n1 _rinde_klapa_kierunek1 = %%;\
+        /test echo(decode_attr("          #    "), "Cwhite") %%;\
+        /test echo(decode_attr("           #   "), "Cwhite") %%;\
+        /test echo(decode_attr("    #########  "), "Cwhite") %%;\
+        /test echo(decode_attr("           #   "), "Cwhite") %%;\
+        /test echo(decode_attr("          #    "), "Cwhite") %%;\
+        /set _map_room_ruiny_panika=%%{_map_room_ruiny_panika_d}%%;\
+        /purge _rinde_klapa_kierunek*%;\
+        \
+    /def -msubstr -t'poludniowy-wschod, polnoc i zachod' -n1 _rinde_klapa_kierunek2 = %%;\
+        /test echo(decode_attr("       #       "), "Cwhite") %%;\
+        /test echo(decode_attr("      ###      "), "Cwhite") %%;\
+        /test echo(decode_attr("     # # #     "), "Cwhite") %%;\
+        /test echo(decode_attr("       #       "), "Cwhite") %%;\
+        /test echo(decode_attr("       #       "), "Cwhite") %%;\
+        /set _map_room_ruiny_panika=%%{_map_room_ruiny_panika_c}%%;\
+        /purge _rinde_klapa_kierunek*%;\
+        \
+    /def -msubstr -t'poludniowy-zachod, polnocny-wschod i zachod' -n1 _rinde_klapa_kierunek3 = %;\
+        /test echo(decode_attr("     #         "), "Cwhite") %%;\
+        /test echo(decode_attr("    #          "), "Cwhite") %%;\
+        /test echo(decode_attr("   ##########  "), "Cwhite") %%;\
+        /test echo(decode_attr("    #          "), "Cwhite") %%;\
+        /test echo(decode_attr("     #         "), "Cwhite") %%;\
+        /set _map_room_ruiny_panika=%%{_map_room_ruiny_panika_e}%%;\
+        /purge _rinde_klapa_kierunek*%;\
+        \
+    /def -msubstr -t'polnocny-wschod, polnoc, poludniowy-zachod, wschod i zachod' -n1 _rinde_klapa_kierunek4 = %;\
+        /test echo(decode_attr("        # # #  "), "Cwhite") %%;\
+        /test echo(decode_attr("          # #  "), "Cwhite") %%;\
+        /test echo(decode_attr("        #   #  "), "Cwhite") %%;\
+        /test echo(decode_attr("      #        "), "Cwhite") %%;\
+        /test echo(decode_attr("    #          "), "Cwhite") %%;\
+        /set _map_room_ruiny_panika=%%{_map_room_ruiny_panika_i}%%;\
+        /purge _rinde_klapa_kierunek*%;\
+    podnies klape
 
-/def -msubstr -t'poludniowy-wschod, polnoc i zachod' -n1 _rinde_klapa_kierunek2 = \
-	/test echo(decode_attr("       #       "), "Cwhite") %;\
-	/test echo(decode_attr("      ###      "), "Cwhite") %;\
-	/test echo(decode_attr("     # # #     "), "Cwhite") %;\
-	/test echo(decode_attr("       #       "), "Cwhite") %;\
-	/test echo(decode_attr("       #       "), "Cwhite") %;\
-	/purge _rinde_klapa_kierunek*
-
-/def -msubstr -t'poludniowy-zachod, polnocny-wschod i zachod' -n1 _rinde_klapa_kierunek3 = \
-	/test echo(decode_attr("     #         "), "Cwhite") %;\
-	/test echo(decode_attr("    #          "), "Cwhite") %;\
-	/test echo(decode_attr("   ##########  "), "Cwhite") %;\
-	/test echo(decode_attr("    #          "), "Cwhite") %;\
-	/test echo(decode_attr("     #         "), "Cwhite") %;\
-	/purge _rinde_klapa_kierunek*
-
-/def -msubstr -t'polnocny-wschod, polnoc, poludniowy-zachod, wschod i zachod' -n1 _rinde_klapa_kierunek4 = \
-	/test echo(decode_attr("        # # #  "), "Cwhite") %;\
-	/test echo(decode_attr("          # #  "), "Cwhite") %;\
-	/test echo(decode_attr("        #   #  "), "Cwhite") %;\
-	/test echo(decode_attr("      #        "), "Cwhite") %;\
-	/test echo(decode_attr("    #          "), "Cwhite") %;\
-	/purge _rinde_klapa_kierunek*
-
-
-;Ta dosc duza plaskorzezba wykonana jest z zoltego piaskowca i przedstawia pysk kozla. I chociaz rzezba mocno juz jest zniszczona wciaz mozna poznac, ze jej tworca
-;    staral sie jak najwierniej oddac szczegoly, mozna nawet rozpoznac poszczegolne pasma brody zwierzecia. W swym rozwartym pysku koziol trzyma stalowa obrecz,
-;    bedaca pierwszym ogniwem poteznego lancucha, ktory ukosem pnie sie ku gorze i znika w ciemnym otworze na srodku sufitu.
